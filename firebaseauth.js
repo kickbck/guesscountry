70bae11a26262efe8b74e863253512ad2b1c82eb
Anola Pilgrim <anola.pilgrim@gmail.com>
2025-04-17 22:37:01 -0400

    fixed sign up

--- firebaseauth.js
+++ firebaseauth.js
@@ -1,7 +1,7 @@
 //SDKs from firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
-
+import { setDoc, doc, getFirestore } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
 
 // firebase config
 const firebaseConfig = {
@@ -16,6 +16,7 @@ const firebaseConfig = {
 // initialize firebase + auth
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
 
 function showMessage(message, divId) {
